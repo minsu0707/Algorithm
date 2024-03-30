784044c91159079bc0320365d8a39a3d8bd2843d
민수 <162300458+min-su08@users.noreply.github.com>
2024-03-30 22:35:49 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 376 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int n;
+  int num1, num2;
+  int total = 0;
+  scanf("%d", &n);
+
+  for(int i=0; i<n; i++)
+    {
+      scanf("%d %d", &num1, &num2);
+      total = num1 + num2;
+      printf("%d\n", total);
+    }
+
+  return 0;
+}
