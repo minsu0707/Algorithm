dea0a7aa440f517144a5a4d78b46a36efb45c09f
민수 <162300458+min-su08@users.noreply.github.com>
2024-04-23 08:36:03 +0900

    [Bronze V] Title: 자동완성, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+  char input;
+
+  scanf("%c", &input);
+
+  if(input == 'N' || input == 'n')
+  {
+    printf("Naver D2");
+  }
+    
+  else 
+  {
+    printf("Naver Whale");
+  }
+  
+  return 0;
+}
