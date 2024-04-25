385bb631801ad85c83a2e910a29aeaf732940c30
민수 <162300458+min-su08@users.noreply.github.com>
2024-04-26 08:28:36 +0900

    [Bronze IV] Title: 평균 점수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int total = 0, i, tmp;
+    for(i = 0; i < 5; i++){
+        scanf("%d", &tmp);
+        if(tmp < 40){
+            tmp = 40;
+        }
+        total += tmp;
+    }
+    printf("%d", total/5);
+    return 0;
+}
