b81a99af9c05bd8c62af9daafccc2cbf99565c48
민수 <162300458+min-su08@users.noreply.github.com>
2024-04-30 14:16:04 +0900

    [Bronze V] Title: Serca, Time: 132 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+
+  int N;
+
+  scanf("%d", &N);
+
+  for (int i = 0; i < N; i++) {
+
+    printf(" @@@   @@@ \n");
+    printf("@   @ @   @\n");
+    printf("@    @    @\n");
+    printf("@         @\n");
+    printf(" @       @ \n");
+    printf("  @     @  \n");
+    printf("   @   @   \n    @ @    \n     @     \n");
+  
+  }
+
+  return 0;
+}
