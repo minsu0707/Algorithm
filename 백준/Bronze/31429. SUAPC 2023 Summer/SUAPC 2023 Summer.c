e50727b932184b01c522b6e8c2ba8cbd3f25264b
민수 <162300458+min-su08@users.noreply.github.com>
2024-05-01 08:36:34 +0900

    [Bronze V] Title: SUAPC 2023 Summer, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/31429. SUAPC 2023 Summer/SUAPC 2023 Summer.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+
+  int N;
+  scanf("%d", &N);
+
+  if(N == 1) printf("12 1600");
+  if(N == 2) printf("11 894");
+  if(N == 3) printf("11 1327");
+  if(N == 4) printf("10 1311");
+  if(N == 5) printf("9 1004");
+  if(N == 6) printf("9 1178");
+  if(N == 7) printf("9 1357");
+  if(N == 8) printf("8 837");
+  if(N == 9) printf("7 1055");
+  if(N == 10) printf("6 556");
+  if(N == 11) printf("6 773");
+
+
+  return 0;
+}
