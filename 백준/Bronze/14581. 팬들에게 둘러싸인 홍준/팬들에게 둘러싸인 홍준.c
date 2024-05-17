6fc6713caa67a3c3a4e647df895f191ddb6c8d2c
민수 <162300458+min-su08@users.noreply.github.com>
2024-05-17 23:24:38 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+  char s[21];
+  scanf("%s", s);
+
+  printf(":fan::fan::fan:\n");
+  printf(":fan::%s::fan:\n", s);
+  printf(":fan::fan::fan:\n");
+  
+  return 0;
+}
