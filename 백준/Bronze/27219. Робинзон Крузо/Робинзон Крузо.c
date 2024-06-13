83e81518a4c30b19429040014ff6e3948444f4fd
최민수 <162300458+min-su08@users.noreply.github.com>
2024-06-14 08:55:00 +0900

    [Bronze V] Title: Робинзон Крузо, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/27219. Робинзон Крузо/Робинзон Крузо.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+  int n;
+  scanf("%d", &n);
+
+  for(int i=0; i<n/5; i++) {
+    printf("V");
+  }
+  for(int i=0; i<n%5; i++) {
+    printf("I");
+  }
+  
+  return 0;
+}
