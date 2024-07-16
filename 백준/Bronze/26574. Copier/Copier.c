d5145d1f727b04b0fadbaf496aba91a25eb40221
최민수 <162300458+min-su08@users.noreply.github.com>
2024-07-17 08:07:14 +0900

    [Bronze V] Title: Copier, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/26574. Copier/Copier.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    
+    int n;
+    scanf("%d", &n);
+    int num;
+    
+    for(int i=0; i<n; i++) {
+        scanf("%d", &num);
+        printf("%d %d\n", num, num);
+    }
+    
+    return 0;
+}
