7dc0dd9c2cd52e6f827d34c31bb7c149f3a7eed6
최민수 <162300458+min-su08@users.noreply.github.com>
2024-09-10 15:15:48 +0900

    [Bronze V] Title: Can you add this?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int n, num1, num2;
+    cin >> n;
+    for(int i=0; i<n; i++) {
+        cin >> num1 >> num2;
+        cout << num1 + num2 << "\n";
+    }
+    
+    return 0;
+}
