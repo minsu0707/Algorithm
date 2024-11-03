1f87dcd7dcf5285a0bef59841582d99f108b7677
최민수 <162300458+minsu0707@users.noreply.github.com>
2024-11-03 20:10:52 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
@@ -1,8 +1,8 @@
 function solution(n) {
     let result = 0;
-    for(let i = 1; i <= 6*n; i++) {
-        if(6 * i % n === 0) {
-            result += i;
+    for(let i=1; i <= n * 6; i++) {
+        if(i * 6 % n === 0) {
+            result = i;
             break;
         }
     }
