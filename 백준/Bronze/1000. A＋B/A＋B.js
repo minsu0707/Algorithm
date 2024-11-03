0674c19749cc5e2d61f274ea6aa8b86175c0a762
최민수 <162300458+minsu0707@users.noreply.github.com>
2024-11-04 08:26:28 +0900

    [Bronze V] Title: A+B, Time: 96 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.js
+++ 백준/Bronze/1000. A＋B/A＋B.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().split(' ');
+let input = fs.readFileSync('/dev/stdin').toString().trim().split(' ');
 let a = parseInt(input[0]);
 let b = parseInt(input[1]);
 console.log(a+b);
