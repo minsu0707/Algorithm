a9a0379e87eacb48ef1399c5c1b01d714f0b7e69
최민수 <162300458+minsu0707@users.noreply.github.com>
2025-09-28 21:37:15 +0900

    [Bronze V] Title: M-Climb, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/34411. M－Climb/M－Climb.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    int m, n, y;
+    scanf("%d", &m);
+    scanf("%d", &n);
+    scanf("%d", &y); // 연도는 사용하지 않음
+
+    printf("%d\n", m * n);
+    return 0;
+}
