49aa06d5099f8dd5aa7ef76690b45cc70efefa32
최민수 <162300458+minsu0707@users.noreply.github.com>
2025-10-27 17:04:03 +0900

    [Bronze V] Title: 시험 성적, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+score = int(input())
+
+if score >= 90:
+    grade = "A"
+elif 90 > score >= 80:
+    grade = "B"
+elif 80 > score >= 70:
+    grade = "C"
+elif 70 > score >= 60:
+    grade = "D"
+else: grade = "F"
+print(grade)
