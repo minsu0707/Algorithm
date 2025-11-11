2e009b3b1078b5aeaa7027497306e729b578e453
최민수 <162300458+minsu0707@users.noreply.github.com>
2025-11-11 14:27:36 +0900

    [Bronze V] Title: 문자열, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/9086. 문자열/문자열.py
+++ 백준/Bronze/9086. 문자열/문자열.py
@@ -3,6 +3,4 @@ import sys
 T = int(sys.stdin.readline())
 for _ in range(T):
     word = sys.stdin.readline().strip()
-    word_arr = list(word)
-    last_idx = len(word_arr)-1
-    print(f'{word_arr[0]}{word_arr[last_idx]}')
+    print(word[0] + word[-1])
