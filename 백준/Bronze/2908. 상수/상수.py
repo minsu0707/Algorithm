676cb9dda4de0059834527bc5f483e9e4032c35e
최민수 <162300458+minsu0707@users.noreply.github.com>
2025-12-14 21:08:13 +0900

    [Bronze II] Title: 상수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.py
+++ 백준/Bronze/2908. 상수/상수.py
@@ -1,9 +1,2 @@
 nums = input().split()
-bigger = 0
-
-for i in range(len(nums)):
-    reversed_num = int("".join(reversed(nums[i])))
-    if bigger < reversed_num:
-        bigger = reversed_num
-
-print(bigger)
+print(max(int(num[::-1]) for num in nums))
