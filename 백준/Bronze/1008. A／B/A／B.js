dc28e344fbb7b5dc23872b8c21ebfb2c6bb999e6
최민수 <162300458+minsu0707@users.noreply.github.com>
2026-01-05 11:38:19 +0900

    [Bronze V] Title: A/B, Time: 96 ms, Memory: 9604 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.js
@@ -0,0 +1,10 @@
+//https://www.acmicpc.net/problem/1008
+
+const fs = require("fs");
+
+const input = fs.readFileSync("/dev/stdin").toString().split(' ');
+
+const A = parseInt(input[0]);
+const B = parseInt(input[1]);
+
+console.log(A / B);
